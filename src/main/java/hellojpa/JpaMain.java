68eeedf93d69d3fd4d1a820ca6be47e4ac95fb5c
최선혁 <wafy75@gmail.com>
2022-10-24 13:41:30 +0900

    update 실행 해보기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -17,12 +16,9 @@ public class JpaMain {
         try {
 
             //영속
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
-
-            em.persist(member1);
-            em.persist(member2);
-            System.out.println("=============");
+            Member findMember = em.find(Member.class, 150L);
+            findMember.setName("ZZZZZ");
+            System.out.println("===========");
 
             tx.commit();
         } catch (Exception e) {
