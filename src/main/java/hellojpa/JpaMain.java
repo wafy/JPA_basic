4f56b2d9594b9dc71e82f032cbe021ca95e147b4
최선혁 <wafy75@gmail.com>
2022-10-25 13:46:29 +0900

    Mapped Superclass

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -15,24 +16,14 @@ public class JpaMain {
 
         try {
 
-            Movie movie = new Movie();
-            movie.setDirector("A");
-            movie.setActor("BBB");
-            movie.setName("바람과 함께 사라지다");
-            movie.setPrice(10000);
-            em.persist(movie);
-
-            Album album = new Album();
-            album.setName("마마무");
-            album.setPrice(10000);
-
-            em.persist(album);
+            Member member = new Member();
+            member.setUsername("choi");
+            member.setCreatedBy("Ryan");
+            member.setCreatedByDate(LocalDateTime.now());
 
             em.flush();
             em.clear();
 
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         } catch (Exception e) {
