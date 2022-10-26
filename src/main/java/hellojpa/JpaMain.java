8dbe812db3f2f5f9f6b4c1fa506b6d179be71c21
최선혁 <wafy75@gmail.com>
2022-10-26 10:50:48 +0900

    casecade 전파

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,25 +16,14 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member1 = new Member();
-            member1.setUsername("hello1");
-            member1.setTeam(team);
-            em.persist(member1);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            em.flush();
-            em.clear();
-
-            Member m = em.find(Member.class, member1.getId());
-            System.out.println("m.getClass() = " + m.getTeam().getClass());
-
-            System.out.println("===============");
-            m.getTeam().getName();
-            System.out.println("===============");
-
+            em.persist(parent);
 
             tx.commit();
         } catch (Exception e) {
